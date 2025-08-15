80f1d600beb4c1aad0c10219542e89dcb2c327be
farhad <farhadhsn8@gmail.com>
2025-08-15 07:55:40 +0000

    add voice and cors err

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.apis.v1.route import app as api_app
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # http://127.0.0.1:5682/api/docs
 # ps aux | grep uvicorn
 # kill 376300
@@ -7,4 +8,11 @@ from fastapi import FastAPI
 # nohup uvicorn main:app --host 0.0.0.0 --port 5682 --workers 1 > logs/output.log 2>&1 &
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins (adjust in production)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 app.mount("/api", api_app)
--- src/services/reporters/Reporter_from_voice.py
+++ src/services/reporters/Reporter_from_voice.py
@@ -1,6 +1,7 @@
+from pydub import AudioSegment
 from src.services.reporters.Reporter import Reporter
 from transformers import pipeline
-
+import os 
 
 class Reporter_from_voice:
     def __init__(self):
@@ -13,6 +14,21 @@ class Reporter_from_voice:
         )
 
 
+
+    def convert_to_mp3(self, input_file_path, output_file_path):
+
+        """
+        Convert an audio file to MP3 format.
+
+        :param input_file_path: Path to the input audio file.
+        :param output_file_path: Path where the output MP3 file will be saved.
+        """
+        # Load the audio file
+        audio = AudioSegment.from_file(input_file_path)
+        # Export as MP3
+        audio.export(output_file_path, format="mp3")
+
+
     def speech_to_text(self, audio_path: str, language: str = "en") -> str:        
         # Process audio with language specification
         result = self.pipe(
@@ -23,7 +39,10 @@ class Reporter_from_voice:
 
     
     def generate_report(self, audio_path: str, report_type: str) -> str:
-        gen_text = self.speech_to_text(audio_path)
+        file_addr = os.path.splitext(audio_path)[0] + ".mp3"
+        self.convert_to_mp3(audio_path,file_addr)
+
+        gen_text = self.speech_to_text(file_addr)
         return self.text_reporter.generate_report(gen_text, report_type)
 
         
